486d7e9ef63bd5268d378b2d65b0d82caff5519b
Ron Gilson <rgilson@t1v.com>
2016-01-30 00:55:17 -0500

    add gameManager stubs

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+	//Clears our list gridPositions and prepares it to generate a new board.
+	void InitialiseList ()
+	{
+		
+	}
+
+
+	//Sets up the outer walls and floor (background) of the game board.
+	void BoardSetup ()
+	{
+		//Instantiate Board and set boardHolder to its transform.
+		//boardHolder = new GameObject ("Board").transform;
+
+
+	}
+
+	//SetupScene initializes our level and calls the previous functions to lay out the game board
+	public void SetupScene (int level)
+	{
+		//Creates the outer walls and floor.
+		BoardSetup ();
+
+		//Reset our list of gridpositions.
+		InitialiseList ();
+
+
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using Time;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance = null;
+
+	public int score;
+	public var totalTime;
+
+	public static GameManager GetInstance()
+	{
+		if(instance == null)
+		{
+			instance = new GameManager();
+		}
+
+		return instance;
+	}
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		//Check if instance already exists
+		if (instance == null)
+
+			//if not, set instance to this
+			instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+
+		//Get a component reference to the attached BoardManager script
+		//boardScript = GetComponent<BoardManager>();
+
+		//Call the InitGame function to initialize the first level 
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		//Set level, setup scene
+		score = 0;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		totalTime += Time.deltaTime;
+	}
+
+	void addCollectible(int value){
+		score += value;
+	}
+
+	int getScore(){
+		
+		return score;
+	}
+}

c77668dab7ed6c0a76c8590ce452c5010eb4dbbc
Ron Gilson <rgilson@t1v.com>
2016-01-30 01:43:23 -0500

    board timer working

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -2,7 +2,20 @@
 using System;
 using System.Collections.Generic;
 
+public enum ballModifier{
+	bounceNone, bounceShort, bounceNormal, bounceHigh,
+	sizeSmall, sizeNormal, sizeLarge,
+	speedSlow, speedNormal, speedFast,
+	gravityLeft, gravityUp, gravityRight, gravityDown
+
+};
+
 public class BoardManager : MonoBehaviour {
+
+	public int modifierDuration = 5;
+	public int[] modifierList;
+	public int modifierPosition = 0;
+	public float totalTime;
 
 	//Clears our list gridPositions and prepares it to generate a new board.
 	void InitialiseList ()
@@ -10,8 +23,26 @@ public class BoardManager : MonoBehaviour {
 		
 	}
 
+	void Awake(){
+		totalTime = 0;
+	}
 
-	//Sets up the outer walls and floor (background) of the game board.
+	// Update is called once per frame
+	void Update () {
+		totalTime += Time.deltaTime;
+
+		Debug.Log("Update Time = " + totalTime + " ____ " + modifierPosition);
+		if (totalTime > modifierDuration * (modifierPosition + 1)) {
+			//send the calculated position
+			modifierPosition += 1;
+		}
+		if (totalTime >= modifierDuration * modifierList.Length) {
+			//End level
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+
+
 	void BoardSetup ()
 	{
 		//Instantiate Board and set boardHolder to its transform.

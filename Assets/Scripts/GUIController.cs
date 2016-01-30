317b0a43d8583305f31ea235629e6d2d6e66da34
Ron Gilson <rgilson@t1v.com>
2016-01-30 12:19:05 -0500

    initial GUIController stub

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+
+	public ballModifier[] modifierList;
+
+	public static GUIController instance = null;
+	public static GUIController GetInstance()
+	{
+		if(instance == null)
+		{
+			instance = new GUIController();
+		}
+
+		return instance;
+	}
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		//Check if instance already exists
+		if (instance == null)
+
+			//if not, set instance to this
+			instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+
+		//Get a component reference to the attached BoardManager script
+		//boardScript = GetComponent<BoardManager>();
+
+		//Call the InitGame function to initialize the first level 
+		InitGame();
+	}
+
+	void InitGame()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

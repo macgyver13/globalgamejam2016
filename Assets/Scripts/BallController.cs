cfc5dd8f727f95aec6db41131e56b1feffaf26b5
Benjamin Hamrick <benthesim@gmail.com>
2016-01-29 21:08:22 -0500

    initial commit.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Floor")
+        {
+            Rigidbody2D rigidbody = transform.GetComponent<Rigidbody2D>();
+            transform.GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+        }
+    }
+}

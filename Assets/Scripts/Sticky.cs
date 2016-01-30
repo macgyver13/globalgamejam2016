500188fbcc1b866c5b7af2639e1b3627552ddd9d
destriu <demcdougald@yahoo.com>
2016-01-30 16:08:48 -0500

    Adding prefab for Platforms tha are now sticky and the sticky script

---
+++ Assets/Scripts/Sticky.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sticky : MonoBehaviour {
+
+    public MovingPlatformController mpController;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log("Trigger entered");
+        mpController.StickyTrigger(coll, GetComponent<RelativeJoint2D>());
+    }
+}

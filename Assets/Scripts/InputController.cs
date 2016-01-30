504007487ad88a662b34b32ed11901bd1423c327
davecdempsey <davecdempsey@gmail.com>
2016-01-30 09:15:32 -0500

    Touch Input REALLY added this time

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,62 +3,68 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class InputController : MonoBehaviour {
-
+	
 	public int currentTouch;
-
-	BallController ballController;
-
+	
+	public BallController ballController;
+	
+	private enum Control {
+		Right,
+		Left,
+		None
+	};
+	private Control currentControl;
+	
 	// Use this for initialization
 	void Start () {
-        ballController = GetComponent<BallController>();
-        currentTouch = -1;
+		currentTouch = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
-//		foreach (Touch touch in Input.touches) {
-//			print ("TOUCH");
-//			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
-//				if(currentTouch == touch.fingerId) {
-//					touch = currentTouch;
-//					CaculateTouch();
-//				}
-//			}
-//			else {
-//				if(touch == currentTouch && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
-//					currentTouch = null;
-//				}
-//			}
-//		}
-#else
-		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-			Right();
+		#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
+				if(currentControl == Control.None) {
+					currentTouch = touch.fingerId;
+					CaculateTouch(touch);
+				}
+			}
+			else {
+				if(touch.fingerId == currentTouch && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
+					currentControl = Control.None;
+				}
+			}
 		}
-		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-			Left();		
+		#else
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+			Right ();
+		} else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+			Left ();		
+		} else {
+			currentControl = Control.None;
 		}
-#endif
+		#endif
 	}
-
+	
 	void CaculateTouch(Touch touch) {
 		float mid = Screen.width * 0.5f;
 		if (touch.position.x < mid) {
-			print("LEFT");
 			Left ();
 		}
 		else {
-			print("RIGHT");
 			Right ();
 		}
 	}
-
-
+	
+	
 	void Right() {
 		ballController.Right ();
+		currentControl = Control.Right;
 	}
-
+	
 	void Left () {
 		ballController.Left ();
+		currentControl = Control.Left;
 	}
 }

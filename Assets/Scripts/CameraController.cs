4498392526cc3484eebd0bab2d3c0898829a8997
davecdempsey <davecdempsey@gmail.com>
2016-01-29 23:24:51 -0500

    CAMERA CONTROLLER WORKS!!!!

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,7 +11,6 @@ public class CameraController : MonoBehaviour {
 	public float cameraHeight = 2.5f; // height of camera adjustable
 	public Vector2 velocity; // speed of camera movement
 	private float currentYPosition;
-	public float threshHold = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -20,53 +19,40 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		print ("----------Update----------");
-		print ("Screen.height: " + Screen.height);
-		print ("Screen.width: " + Screen.width);
-		print ("Ball Position: " + ball.position);
-		Vector3 potentialNewPoint = new Vector3(Mathf.SmoothDamp(_transform.position.x, ball.position.x, ref velocity.x, smoothTime), Mathf.SmoothDamp(_transform.position.y, ball.position.y, ref velocity.y, smoothTime), _transform.position.z);
+		currentYPosition = _transform.position.y;
+		Vector3 potentialNewPoint = new Vector3(Mathf.SmoothDamp(_transform.position.x, ball.position.x, ref velocity.x, smoothTime), 
+		                                        Mathf.SmoothDamp(_transform.position.y, ball.position.y, ref velocity.y, smoothTime), 
+		                                        _transform.position.z);
 
 		float potentialNewY = potentialNewPoint.y;
 		float topThresh = TopThreshHold ();
 		float bottomThresh = BottomThreshHold ();
 		float newYPosition = currentYPosition;
 
-		print ("topThresh :" + topThresh);
-		print ("bottomThresh: " + bottomThresh);
-		print ("potentialNewY: " + potentialNewY);
 		if (ball.position.y > topThresh) {
-			print ("potentialNewY > TopThresh");
 			newYPosition = potentialNewY;
 		}
 		if (ball.position.y < bottomThresh) {
-			print ("potentialNewY < bottomThresh");
 			newYPosition = potentialNewY;
 		}
-			
-		_transform.position = new Vector3(Mathf.SmoothDamp(_transform.position.x, ball.position.x, ref velocity.x, smoothTime), newYPosition, _transform.position.z);
+
+		transform.position = new Vector3(Mathf.SmoothDamp(_transform.position.x, ball.position.x, ref velocity.x, smoothTime), newYPosition, _transform.position.z);
 	}
 
 	float TopThreshHold () {
-		print ("----------TopThreshHold----------");
-		float fourth = Screen.height * 0.10f;
+		float fourth = Screen.height * 0.40f;
 		float thresh = Screen.height - fourth;
 		Vector3 screenPos = new Vector3 (0, thresh, 0);
 		Vector3 worldThresh = mainCamera.ScreenToWorldPoint (screenPos);
-
-		print ("Screen.height :" + Screen.height);
-		print ("worldThresh.y :" + worldThresh.y);
 
 		return worldThresh.y;
 	}
 
 	float BottomThreshHold () {
-		print ("----------BottomThreshHold----------");
-		float fourth = Screen.height * 0.10f;
+		float fourth = Screen.height * 0.40f;
 		Vector3 screenPos = new Vector3 (0, fourth, 0);
 		Vector3 worldThresh = mainCamera.ScreenToWorldPoint (screenPos);
 
-		print ("fourth :" + fourth);
-		print ("worldThresh.y :" + worldThresh.y);
 		return worldThresh.y;
 	}
 }

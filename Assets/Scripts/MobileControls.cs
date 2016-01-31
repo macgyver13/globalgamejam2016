909911ff073c991b326198d23d256d6124800b11
davecdempsey <davecdempsey@gmail.com>
2016-01-31 15:06:34 -0500

    David's Final Commit

--- Assets/Scripts/MobileControls.cs
+++ Assets/Scripts/MobileControls.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class MobileControls : MonoBehaviour {
+	public GameObject leftButton;
+	public GameObject rightButton;
 
 	// Use this for initialization
 	void Start () {
-	
+		#if (!UNITY_IOS || !UNITY_ANDROID)
+		leftButton.SetActive(false);
+		rightButton.SetActive (false);
+		#endif
 	}
 	
 	// Update is called once per frame

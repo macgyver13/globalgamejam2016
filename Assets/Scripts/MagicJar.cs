dbdbca2df2f80f04d9a02e3f21f71334b0c8e528
destriu <demcdougald@yahoo.com>
2016-01-31 02:50:46 -0500

    Added the script MagicJar

---
+++ Assets/Scripts/MagicJar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicJar : MonoBehaviour {
+    public Transform TeleportLocation;
+
+    private GameObject objectToTele;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        GameObject otherObject = coll.gameObject;
+        if (otherObject.tag == "Ball")
+        {
+            objectToTele = otherObject;
+            objectToTele.GetComponent<Rigidbody2D>().isKinematic = true;
+            Hashtable args = iTween.Hash("alpha", 0f, "time", 1.5f);
+            iTween.FadeTo(otherObject, args);
+
+            StartCoroutine(Teleport());
+        }
+    }
+
+   IEnumerator Teleport()
+    {
+        yield return new  WaitForSeconds(1.5f);
+        objectToTele.transform.position = TeleportLocation.position;
+        Hashtable args = iTween.Hash("alpha", 1f, "time", 1.5f);
+        iTween.FadeTo(objectToTele, args);
+        yield return new WaitForSeconds(1.5f);
+        objectToTele.GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+}
